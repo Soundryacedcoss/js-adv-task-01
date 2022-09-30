e80a9874764c55b7d079f88bda9db80b1f90e4e0
Soundryacedcoss <soundryarani@cedcoss.com>
2022-09-30 18:41:23 +0530

    Modification done

--- task-9/script.js
+++ task-9/script.js
@@ -1,41 +1,42 @@
-var arr=[]
-var data1=document.getElementById("id").value;
-var data2=document.getElementById("name1").value;
-var data2=document.getElementById("sal").value;
-  function fun(){
-    
-    try {
-      if(data2=="") 
-      {
-      throw "Empty";
-      }
-      if(!isNaN(data1))
-      {
-        throw "you"
-      }
-      var obj={
-        data1:id,
-        data2:name1,
-        data3:sal
-      }
-      arr.push(obj)
+var arr = [];
+
+function add() {
+  var id = document.getElementById("id").value;
+  var name1 = document.getElementById("name1").value;
+  var salary = document.getElementById("sal").value;
+
+  try {
+    if (isNaN(id)) throw "id sould be integer";
+    if(id=="") throw "id can not be empty"
+    if (name1 == "") throw "Name can not to be Empty";
+    if (isNaN(salary)) throw "salary sould be integer";
+    if(salary=="") throw "salary can not be empty"
+    else {
+      var obj = {
+        id: id,
+        name1: name1,
+        salary: salary,
+      };
+      arr.push(obj);
+      display();
     }
-       catch (error) {
-      document.getElementById("p1").innerHTML= "input is"+error 
-    }
-    var table="<table><tr><th>id</th><th>name</th><th>Salary</th></tr>"
-    arr.forEach((element) => {
-      table+=
-      '<tr><td>'+
-        element.data1 +
-        '</td><td>' +
-        element.data2 +
-        '</td><td>'
-        +element.data3+
-        '</td></tr>'
-    
-    });
-    table+="</table>"
-    document.getElementById("Demo1").innerHTML=table;
-    
+  } catch (error) {
+    document.getElementById("p1").innerHTML = error;
   }
+}
+
+function display() {
+  var table = "<table><tr><th>id</th><th>name</th><th>Salary</th></tr>";
+  arr.forEach((element) => {
+    table +=
+      "<tr><td>" +
+      element.id +
+      "</td><td>" +
+      element.name1 +
+      "</td><td>" +
+      element.salary +
+      "</td></tr>";
+  });
+  table += "</table>";
+  document.getElementById("Demo1").innerHTML = table;
+}
